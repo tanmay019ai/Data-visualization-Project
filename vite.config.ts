ad56d73ae911b458420980fdf4011fa01bdfdaea
tanmay019ai <tanmaysr019@gmail.com>
2024-12-27 02:26:45 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/host-1",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
